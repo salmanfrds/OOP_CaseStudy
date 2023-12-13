302f1464d72c0c0ffedfbf21d08e1b58fdf4e970
Firdaus Salman <salmanfird4@outlook.com>
2023-12-13 20:19:29 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,4 +1,19 @@
 public class main{
+  public static double calculateSalary(Employee employee){
 
-  
+        var hour = employee.calculateHours();
+        var salary = hour * 6 * 0.89;
+        return salary;
+
+    }
+    public static void main(String[] args) throws Exception {
+        var employee = new Employee();
+
+        employee.clockIn();
+        Thread.sleep(1000);
+        employee.clockOut();
+
+        var salary = calculateSalary(employee);
+        System.out.println(salary);
+    }
 }
