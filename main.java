770435600a4a6a9e33701626457268fda32f0eb8
Firdaus Salman <salmanfird4@outlook.com>
2023-12-15 20:47:25 +0800

    Update main class

--- main.java
+++ main.java
@@ -1,19 +1,63 @@
-public class main{
-  public static double calculateSalary(Employee employee){
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        var hour = employee.calculateHours();
-        var salary = hour * 6 * 0.89;
-        return salary;
+public class App {
+
+    //hakimi
+    public static void calculateSalary(ArrayList<Employee> employeeList){
 
     }
+
+    //salman
+    private static void checkInOrOut(ArrayList<Employee> employeeList) {
+
+    }
+
+    //naqash
+    private static void regEmployee(ArrayList<Employee> employeeList) {
+
+    }
+
+    private static Employee findEmployee(ArrayList<Employee> employeeList, String employeeID) {
+        for (Employee employee : employeeList) {
+            if (employee.getEmployeeID().equals(employeeID)) {
+                return employee;
+            }
+        }
+        return null;
+    }
+    
     public static void main(String[] args) throws Exception {
-        var employee = new Employee();
+        ArrayList<Employee> employeeList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        employee.clockIn();
-        Thread.sleep(1000);
-        employee.clockOut();
+        while (true) {
+            System.out.println("Main Menu:");
+            System.out.println("1. Check in or Check out");
+            System.out.println("2. Register new employee");
+            System.out.println("3. Calculate Salary");
+            System.out.println("0. Exit");
 
-        var salary = calculateSalary(employee);
-        System.out.println(salary);
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    checkInOrOut(employeeList);
+                    break;
+                case 2:
+                    regEmployee(employeeList);
+                    break;
+                case 3:
+                    calculateSalary(employeeList);
+                    break;
+                case 0:
+                    System.out.println("Exiting program. Goodbye!");
+                    scanner.close();
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
